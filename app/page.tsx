2dd1dc489d914884e641c2f22fdacaefe29f3844
duanhongjin <duanhjlt@163.com>
2025-03-26 14:40:41 +0800

    feat: update appstore download url

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <p className="text-[#262626] text-[40px] font-bold">到饭点儿，轻轻一摇推荐附近的餐馆</p>
         </div>
         <div className="mt-[60px] flex items-center justify-center">
-          <a href="https://apps.apple.com/us/app/memora-read-it-later/id6743344692"><Image src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
+          <a href="https://apps.apple.com/us/app/%E9%A5%AD%E7%82%B9%E5%84%BF-%E6%91%87%E4%B8%80%E6%91%87%E6%8E%A8%E8%8D%90%E9%99%84%E8%BF%91%E7%9A%84%E9%A4%90%E9%A6%86/id6743344692"><Image src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
         </div>
         <div className="flex items-center justify-center">
           <Image className="mt-[71px]" src="/phone.png" alt="download" width={1058} height={613}/>
@@ -29,7 +29,7 @@ export default function Home() {
           <p className="text-[#262626] text-[20px]">精心挑选优质的餐馆，以便每餐都能营养丰富和实惠</p>
         </div>
         <p className="text-[#262626] text-[40px] font-bold mt-[161px]">可在AppStore下载</p>
-        <a href="https://apps.apple.com/us/app/memora-read-it-later/id6743344692" className="mt-[40px]"><Image src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
+        <a href="https://apps.apple.com/us/app/%E9%A5%AD%E7%82%B9%E5%84%BF-%E6%91%87%E4%B8%80%E6%91%87%E6%8E%A8%E8%8D%90%E9%99%84%E8%BF%91%E7%9A%84%E9%A4%90%E9%A6%86/id6743344692" className="mt-[40px]"><Image src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
         <p className="mt-[115px] mb-[43px] text-[14px] text-[#262626]">© CrazyKids Studio. 2025</p>
       </div>
     </div>
