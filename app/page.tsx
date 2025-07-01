581c8aa17f81e5660c1d591d4012ff82884582a1
duanhongjin <duanhjlt@gmail.com>
2025-07-01 10:33:09 +0800

    remove <Image>

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
-import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
     <div className="h-screen w-screen bg-white">
       <div className="bg-[#FFF7E8] px-[86px] pt-[26px] pb-[86px]">
         <div className="flex items-center">
-          <Image src="/logo.png" alt="logo" width={56} height={56} />
+          <img src="/logo.png" alt="logo" width={56} height={56} />
           <h1 className="text-[#262626] text-[26px] font-bold ml-[8px]">饭点儿</h1>
         </div>
         <div className="flex items-center justify-around mt-[82px]">
           <p className="text-[#262626] text-[40px] font-bold">到饭点儿，轻轻一摇推荐附近的餐馆</p>
         </div>
         <div className="mt-[60px] flex items-center justify-center">
-          <a href="https://apps.apple.com/cn/app/%E9%A5%AD%E7%82%B9%E5%84%BF-%E6%91%87%E4%B8%80%E6%91%87%E6%8E%A8%E8%8D%90%E9%99%84%E8%BF%91%E7%9A%84%E9%A4%90%E9%A6%86/id6743344692"><Image src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
+          <a href="https://apps.apple.com/cn/app/%E9%A5%AD%E7%82%B9%E5%84%BF-%E6%91%87%E4%B8%80%E6%91%87%E6%8E%A8%E8%8D%90%E9%99%84%E8%BF%91%E7%9A%84%E9%A4%90%E9%A6%86/id6743344692"><img src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
         </div>
         <div className="flex items-center justify-center">
-          <Image className="mt-[71px]" src="/phone.png" alt="download" width={1058} height={613} />
+          <img className="mt-[71px]" src="/phone.png" alt="download" width={1058} height={613} />
         </div>
       </div>
       <div className="pt-[70px] flex flex-col items-center text-center">
@@ -29,7 +28,7 @@ export default function Home() {
           <p className="text-[#262626] text-[20px]">精心挑选优质的餐馆，以便每餐都能营养丰富和实惠</p>
         </div>
         <p className="text-[#262626] text-[40px] font-bold mt-[161px]">可在AppStore下载</p>
-        <a href="https://apps.apple.com/cn/app/%E9%A5%AD%E7%82%B9%E5%84%BF-%E6%91%87%E4%B8%80%E6%91%87%E6%8E%A8%E8%8D%90%E9%99%84%E8%BF%91%E7%9A%84%E9%A4%90%E9%A6%86/id6743344692" className="mt-[40px]"><Image src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
+        <a href="https://apps.apple.com/cn/app/%E9%A5%AD%E7%82%B9%E5%84%BF-%E6%91%87%E4%B8%80%E6%91%87%E6%8E%A8%E8%8D%90%E9%99%84%E8%BF%91%E7%9A%84%E9%A4%90%E9%A6%86/id6743344692" className="mt-[40px]"><img src="/download_on_the_app_store_btn.svg" alt="download" width={185} height={63} /></a>
         <div className="flex items-center justify-center mt-[115px] mb-[43px] text-[14px] text-[#262626]">
           <Link href="https://www.crazykids.tech">© CrazyKids Studio. 2025</Link>
           <span className="mx-[10px]">•</span>
